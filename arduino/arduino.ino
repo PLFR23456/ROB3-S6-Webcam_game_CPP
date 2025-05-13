8a2b2f2d30bba17fae484ffb538211481848908c
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2025-05-13 15:35:47 +0200

    REF: Rework code logic
    Servo command function and Serial processing are now separated

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -10,6 +10,11 @@ bool programRunning = true;
 // Déclarer le buffer du port serie
 String serialMessage = "";
 
+int commandePosition[2] = {};
+
+int periodeAffichage;
+int compteurAffichage;
+
 void setup() {
   // Configurer le port serie
   Serial.begin(9600);
@@ -17,76 +22,77 @@ void setup() {
   // Attacher les instance de servomoteurs à leur port de commande (signal PWM)
   joint1Servo.attach(5); 
   joint2Servo.attach(6);
+
+  periodeAffichage = 10000;
+  compteurAffichage = 0;
 }
 
-
 void loop() {
+  compteurAffichage++;
+
   if (programRunning) {
-    commanderBras(joint1Servo, joint2Servo);
+    conversionMessageSerieVersCommandeServo(commandePosition);
+    
+    // Affiche les valeurs
+    if (compteurAffichage == periodeAffichage) {
+      // Afficher les commandes
+      Serial.println("Commandes de position angulaire :");
+      Serial.print("baseServo : ");
+      Serial.println(commandePosition[0]);
+      Serial.println("Commandes de position angulaire :");
+      Serial.print("armServo : ");
+      Serial.println(commandePosition[1]);
+
+      compteurAffichage = 0;
+    }
+
+    //commanderBras(joint1Servo, joint2Servo, commandePosition[0], commandePosition[1]);
+    
+  } else {
+    Serial.println("Arrêt des moteurs");
+    joint1Servo.detach();
+    joint2Servo.detach();
   }
 }
 
-void commanderBras(Servo baseServo, Servo armServo) {
+void conversionMessageSerieVersCommandeServo(int servoPos[2]) {
   if (Serial.available()) { // Si un signal est détecté sur le port serie
-    int serialCharacter = Serial.read(); // Récupérer le signal entrant brut (caractères sous format binaire) du port serie dans un buffer 
-    
+    int serialCharacter = Serial.read(); // Récupérer le signal entrant brut (caractères sous format binaire) du port serie dans un buffer
+
     // Ajoute le caractère au buffer (chaîne)
-    if (isDigit(serialCharacter)) serialMessage += (char)serialCharacter;
+    if (isDigit(serialCharacter)) serialMessage += (char)serialCharacter; 
 
     // Traitement du signal
     if (serialCharacter == ' ') { // Si le caractère courant est un espace
       // Convertir le premier mot du message (commande du premier servomoteur) en entier
-      int baseServoPos = serialMessage.toInt();
-      
-      Serial.println("Commandes de position angulaire :");
-      Serial.print("baseServo : ");
-      Serial.println(baseServoPos);
-
-      // Envoyer la commande au servomoteur
-      baseServo.write(baseServoPos);
+      servoPos[0] = serialMessage.toInt();
 
       // Vider le buffer
       serialMessage = "";
     } else if (serialCharacter == '\n') { // Si le caractère courant est un retour à la ligne
       // Convertir le second mot du message (commande du second servomoteur) en entier
-      int armServoPos = serialMessage.toInt();
-      
-      Serial.println("Commandes de position angulaire :");
-      Serial.print("armServo : ");
-      Serial.println(armServoPos);
-
-      // Envoyer la commande au servomoteur
-      armServo.write(armServoPos);
+      servoPos[1] = serialMessage.toInt();
 
       // Vider le buffer
       serialMessage = "";
     }
 
-    if (serialCharacter == '!') {
-      if (serialMessage = "STOP") {
-        programRunning = false;
-        Serial.println("Arrêt des moteurs");
-        baseServo.detach();
-        armServo.detach();
-      }
-    }
-    if (serialCharacter == 't') {
-      Serial.println("Turn mode !");
-      turnMode=!turnMode;
-      baseServo.write(90);
-      armServo.write(90);
-      serialMessage = "";
-    }
-      
-    delay(10);   // waits 15ms for the servo to reach the position
-      
-    if (turnMode) {
-      baseServo.write(90);
-      armServo.write(10);
-      delay(100);
-      armServo.write(90);
-      delay(100);
-      return;  // Ne pas lire le port série pendant le mode "turn"
-    }
+    if ((serialCharacter == '!') && (serialMessage = "STOP")) programRunning = false;
   }
 }
+
+void commanderBras(Servo baseServo, Servo armServo, int baseServoPos, int armServoPos) {
+  // Afficher les commandes
+  Serial.println("Commandes de position angulaire :");
+  Serial.print("baseServo : ");
+  Serial.println(baseServoPos);
+  Serial.println("Commandes de position angulaire :");
+  Serial.print("armServo : ");
+  Serial.println(armServoPos);
+
+  // Envoyer les commandes aux servomoteurs
+  baseServo.write(baseServoPos);
+  armServo.write(armServoPos);
+}
+
+    
